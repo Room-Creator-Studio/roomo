ed2452b8962fe7d52e01a3e7d23f88dac466afbe
ROOM <amrik.sahnsra@gmail.com>
2026-02-27 20:41:10 +0530

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -10,12 +10,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth
 // Firebase configuration
 // Replace these with your actual Firebase project credentials
 const firebaseConfig = {
-  apiKey: "ENV_API_KEY",
-  authDomain: "ENV_AUTH_DOMAIN",
-  projectId: "ENV_PROJECT_ID",
-  storageBucket: "ENV_STORAGE_BUCKET",
-  messagingSenderId: "ENV_MESSAGING_SENDER_ID",
-  appId: "ENV_APP_ID"
+  apiKey: "AIzaSyDGRimvvjyQiSyE_-aFlpIgCo1Ky46hQQs",
+  authDomain: "room-99a46.firebaseapp.com"",
+  projectId: "room-99a46",
+  storageBucket: "room-99a46.firebasestorage.app",
+  messagingSenderId: "122165046155",
+  appId: "1:122165046155:web:60e5bc2651b533a94f1d8e",
+  measurementId: "G-SBXMYFCN3M"
 };
 
 // Initialize Firebase
@@ -29,3 +30,4 @@ export const auth = getAuth(app);
 
 // Export app for reference
 export default app;
+
